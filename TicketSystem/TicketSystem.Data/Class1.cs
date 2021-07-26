6d4e7b6f6baa606c52772916f4ad5a7c098171cc
Matthew <showu760422@gmail.com>
2021-07-26 16:53:07 +0800

    project init

---
+++ TicketSystem/TicketSystem.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TicketSystem.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ TicketSystem/TicketSystem.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TicketSystem.Service
+{
+    public class Class1
+    {
+    }
+}
